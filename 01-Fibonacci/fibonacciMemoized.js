6703e4abba24a48751bec9ad9439ded33465e2cf
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-22 15:07:53 +0530

    Renamed the memoized funtion

--- 01-Fibonacci/fibonacciMemoized.js
+++ 01-Fibonacci/fibonacciMemoized.js
@@ -1,15 +1,15 @@
-const fibonacciRecursive = (n, memo = {}) => {
+const fibonacciMemoized = (n, memo = {}) => {
     if(n in memo) 
         return memo[n]
     if(n <= 2)
         return 1
-    memo[n] = fibonacciRecursive(n-1, memo) + fibonacciRecursive(n-2, memo)
+    memo[n] = fibonacciMemoized(n-1, memo) + fibonacciMemoized(n-2, memo)
 
     return memo[n]
 }
 
 let startTime = Date.now() 
-console.log(fibonacciRecursive(50))
+console.log(fibonacciMemoized(50))
 let endTime = Date.now() 
 
 console.log("Time Taken: ", endTime - startTime)
