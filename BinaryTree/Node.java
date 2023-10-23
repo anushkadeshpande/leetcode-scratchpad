926074c30c256249f23caae348d0a3008d92dab5
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-24 00:00:40 +0530

    Added BinaryTree implementation in java

---
+++ BinaryTree/Node.java
@@ -0,0 +1,32 @@
+package BinaryTree;
+
+class Node {
+    int data;
+
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+
+        this.left = null;
+        this.right = null;
+    }
+
+    @Override
+    public String toString() {
+        return "Node [data=" + data + ", left=" + left + ", right=" + right + "]";
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.right.left = new Node(4);
+
+        System.out.println(root);
+
+    }
+}
