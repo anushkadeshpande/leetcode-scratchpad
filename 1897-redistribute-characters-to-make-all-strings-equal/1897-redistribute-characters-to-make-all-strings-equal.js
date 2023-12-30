e05481c624c2e4f8588d5bf36134a709d6ae8f1d
Anushka <anushkaa5000@gmail.com>
2023-12-30 14:22:28 +0530

    Time: 64 ms (77.46%), Space: 44.8 MB (70.42%) - LeetHub

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string[]} words
+ * @return {boolean}
+ */
+var makeEqual = function(words) {
+    
+    // find total number of chars in words
+    // if num chars % length(words) != 0, return false
+    // else
+    // find occurrences of each character, if all of them % len(words) === 0
+    // return true
+    // else return false
+    
+    let allChars = words.join('')
+    let totalChars = allChars.length
+    
+    if(totalChars % words.length !== 0)
+        return false
+    else {
+        let charMap = {}
+        for(let char of allChars) {
+            if(char in charMap)
+                charMap[char] += 1
+            else
+                charMap[char] = 1
+        }
+        
+        for(let char in charMap) {
+            console.log(charMap[char] % words.length)
+            if(charMap[char] % words.length !== 0)
+                return false
+        }
+        return true
+    }
+};
