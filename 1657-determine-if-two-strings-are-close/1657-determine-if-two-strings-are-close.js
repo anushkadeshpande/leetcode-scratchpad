c6ddf433dc99727769ee356449b7a7117c2580b8
Anushka <anushkaa5000@gmail.com>
2024-01-14 22:42:35 +0530

    Time: 150 ms (66.11%), Space: 53.8 MB (12.23%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
@@ -30,17 +30,6 @@ var closeStrings = function(word1, word2) {
     let word1Chars = getOccurrences(word1)
     let word2Chars = getOccurrences(word2)
     
-    // if(word1Chars.kwys)
-//     for(let o in word1Chars) {
-//         if(o in word2Chars && word1Chars[o] === word2Chars[o]) {
-//             delete word1Chars[o]
-//             delete word2Chars[o]
-//         }
-//     }
-    
-//     if(JSON.stringify(word1Chars) === JSON.stringify(word2Chars))
-//         return true
-
     if((Object.values(word1Chars).sort((a,b) => a -b).toString() === Object.values(word2Chars).sort((a,b) => a -b).toString()) && (Object.keys(word1Chars).sort().toString() === Object.keys(word2Chars).sort().toString()))
         return true
     return false
