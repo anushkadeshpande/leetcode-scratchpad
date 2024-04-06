c4dc13859f70d0b758f0eb81621e3850fbfb6854
Anushka <anushkaa5000@gmail.com>
2024-04-06 18:51:04 +0530

    Time: 312 ms (5.20%), Space: 57.1 MB (57.00%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var minRemoveToMakeValid = function(s) {
+    let stack = [] // to track correctness of paranthesis
+    let count = 0
+    
+    let validString = ""
+    
+    let trailingClosingBraces = 0
+    
+    for(let i=0; i<s.length; i++) {
+       
+        if(s.charAt(i) == '(') {
+            // record the position of opening brace
+            stack.push(i)
+            
+            // replace the opening brace in the string with -, to remember that there is an opening brace here
+            validString += "-"
+        }
+        else if(s.charAt(i) == ')') {
+            let correspondingOpening = stack.pop()
+            
+            if(null!= correspondingOpening)
+                validString = validString.substring(0, correspondingOpening-trailingClosingBraces) + '(' + validString.substring(correspondingOpening+1-trailingClosingBraces) + ')'
+            else
+                trailingClosingBraces ++
+            
+        }
+        else 
+            validString += s.charAt(i)
+    }
+    
+    // remove any extra - in the string
+    validString = validString.replace(new RegExp('-', 'g'),"")
+    
+    return validString
+};
