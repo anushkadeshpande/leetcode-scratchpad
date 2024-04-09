7f132b6cd711f3b2231e8fc472294d70a61b5cb9
Anushka <anushkaa5000@gmail.com>
2024-04-09 15:37:44 +0530

    Time: 57 ms (48.63%), Space: 48.9 MB (69.95%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
@@ -5,17 +5,33 @@
  */
 var timeRequiredToBuy = function(tickets, k) {
     let time = 0
-    while(tickets[k] !=0) {
-        for(let i=0; i<tickets.length; i++) {
-            if(k === i && tickets[i] === 1) {
-                time++
-                return time
-            }
-            else if(tickets[i] >0) {
-                tickets[i] -=1
-                time++
-            }
+
+    // APPROACH - 2    
+    // before k, we'll reduce k number of times
+    // after k, we'll reduce k-1 times
+    
+    for(let i=0; i<tickets.length; i++) {
+        if(i <= k) {
+            time += Math.min(tickets[i], tickets[k])
+        } else {
+            time += Math.min(tickets[i], tickets[k] - 1)
         }
     }
     
+    return time
+    
+    // APPROACH-1
+    
+    // while(tickets[k] !=0) {
+    //     for(let i=0; i<tickets.length; i++) {
+    //         if(k === i && tickets[i] === 1) {
+    //             time++
+    //             return time
+    //         }
+    //         else if(tickets[i] >0) {
+    //             tickets[i] -=1
+    //             time++
+    //         }
+    //     }
+    // }
 };
