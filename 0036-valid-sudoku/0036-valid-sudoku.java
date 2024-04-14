82a331334cb8e4065d0f9c3a4a9db5b6d907e6e2
Anushka <anushkaa5000@gmail.com>
2024-04-14 15:57:56 +0530

    Time: 15 ms (16.24%), Space: 44.5 MB (43.46%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -2,18 +2,13 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         Set<String> sudokuState = new HashSet<>();
         
-        for(int i=0; i<board[0].length; i++) {
-            for(int j=0; j<board.length; j++) {
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
                 if(board[i][j] != '.') {
                     // check if set has the element
-                    if(sudokuState.contains(board[i][j] + " in row " + i) || sudokuState.contains(board[i][j] + " in col " + j) || sudokuState.contains(board[i][j] + " in box " + i/3 + " : " + j/3))
+                    if(!sudokuState.add(board[i][j] + " in row " + i) || !sudokuState.add(board[i][j] + " in col " + j) || !sudokuState.add(board[i][j] + " in box " + i/3 + " : " + j/3))
                         return false;
                     
-                    else {
-                        sudokuState.add(board[i][j] + " in row " + i);
-                        sudokuState.add(board[i][j] + " in col " + j);
-                        sudokuState.add(board[i][j] + " in box " + i/3 + " : " + j/3);
-                    }
                 }
             }
         }
