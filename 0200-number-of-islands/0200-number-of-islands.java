e4eb53725fde5152a2fb827c95dbac41181d4060
Anushka <anushkaa5000@gmail.com>
2024-04-19 19:37:38 +0530

    Time: 3 ms (86.63%), Space: 49.6 MB (41.33%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,16 +1,16 @@
 class Solution {
-    int numIslands = 0;
     public int numIslands(char[][] grid) {
         // traverse through the grid
         // if grid[i][j] == 1, perform dfs and mark the cells as visited
         // do not revisit a cell
+        int numIslands = 0;
         
         for(int i=0; i<grid.length; i++) {
             for(int j=0; j<grid[0].length; j++) {
                 if(grid[i][j] == '1') {
                     // dfs
                     // root == grid[i][j]
-                    grid = dfs(grid, i, j);
+                    dfs(grid, i, j);
                     numIslands++;
                 }
             }
@@ -18,20 +18,18 @@ class Solution {
         return numIslands;
     }
     
-    private char[][] dfs(char[][] grid, int i, int j) {
+    private void dfs(char[][] grid, int i, int j) {
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0' || grid[i][j] == 'v') {
             // numIslands++;
-            return grid;
+            return;
         }
         
         // mark current cell as visited
         grid[i][j] = 'v';
         
-        grid = dfs(grid, i+1, j);
-        grid = dfs(grid, i, j+1);
-        grid = dfs(grid, i-1, j);
-        grid = dfs(grid, i, j-1);
-        
-        return grid;
+        dfs(grid, i+1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j-1);
     }
 }
