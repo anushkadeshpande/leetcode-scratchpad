5be82eccdbc41bce0841cc88c50c67428a5d15d5
Anushka <anushkaa5000@gmail.com>
2024-04-21 16:27:40 +0530

    Time: 211 ms (23.51%), Space: 168.7 MB (25.50%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -7,12 +7,17 @@ class Solution {
         
         // construct graph as adjacency list
         Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int[] edge : edges) {
-            graph.putIfAbsent(edge[0], new ArrayList<>());
-            graph.putIfAbsent(edge[1], new ArrayList<>());
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
-        }
+        // for (int[] edge : edges) {
+        //     graph.putIfAbsent(edge[0], new ArrayList<>());
+        //     graph.putIfAbsent(edge[1], new ArrayList<>());
+        //     graph.get(edge[0]).add(edge[1]);
+        //     graph.get(edge[1]).add(edge[0]);
+        // }
+        
+        Arrays.stream(edges).forEach(edge -> {
+            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+            graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
+        });
         
         // perform dfs
         
