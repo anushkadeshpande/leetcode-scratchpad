01cc227211b4019692fffb0cc55e054eb76da23e
Anushka <anushkaa5000@gmail.com>
2024-05-05 03:06:59 +0530

    Time: 135 ms (6.54%), Space: 45.6 MB (13.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,29 +1,36 @@
 class Solution {
     public boolean isPalindrome(String s) {                
-        int startPointer = 0;
-        int endPointer = s.length() -1;
+        // 1. sanitize the string
+        // 1.1 convert all to lowercase
+        // 1.2 remove all non-alphanumeric characters
+        // 1.3 remove all spaces
         
-        while(startPointer < endPointer) {
-            
-            if(!isAlphaNumeric(s.charAt(startPointer))) 
-                startPointer ++;
-            else if(!isAlphaNumeric(s.charAt(endPointer)))
-                endPointer--;
-            else if(Character.toLowerCase(s.charAt(startPointer)) != Character.toLowerCase(s.charAt(endPointer)))
+        // 2. need to check if it is a palindrome
+        
+        String sanitizedString = "";
+        
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) != ' ' && isAlphaNumeric(s.charAt(i)))
+                sanitizedString += Character.toLowerCase(s.charAt(i));
+        }
+                
+        int i = 0;
+        int j = sanitizedString.length() - 1;
+        
+        while(i < j) {
+            if(sanitizedString.charAt(i) != sanitizedString.charAt(j))
                 return false;
-            
-            else {
-                startPointer ++;
-                endPointer --;
-            }
+            i++;
+            j--;
         }
         
         return true;
     }
     
-    private static boolean isAlphaNumeric(char c) {
-        return (c >= 'a' && c <= 'z') ||
-               (c >= 'A' && c <= 'Z') ||
-               (c >= '0' && c <= '9');
+    private boolean isAlphaNumeric(char c) {
+        return ((c >= 'a' && c <= 'z') 
+                || (c >= 'A' && c <= 'Z') 
+                || (c >= '0' && c <= '9'));
     }
+    
 } 
