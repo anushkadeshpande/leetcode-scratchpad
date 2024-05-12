a5d4da7e4c2515a854b645a08b03672fbbbbf1e2
Anushka <anushkaa5000@gmail.com>
2024-05-13 04:51:40 +0530

    Time: 29 ms (77.57%), Space: 55.3 MB (43.33%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,40 +1,39 @@
 class StockSpanner {
     class Span {
         int price;
-        int idx;
+        int span;
         
-        public Span(int price, int idx) {
+        public Span(int price, int span) {
             this.price = price;
-            this.idx = idx;
+            this.span = span;
         }
         
         public int getPrice() {
             return this.price;
         }
         
-        public int getIdx() {
-            return this.idx;
+        public int getSpan() {
+            return this.span;
         }
     }
     
     Stack<Span> stack;
-    int size;
     
     public StockSpanner() {
         stack = new Stack();
-        size = 0;
+        // size = 0;
     }
     
     public int next(int price) {
         int ans = 1;
-        size ++;
+        // size ++;
         
         while(!stack.empty() && stack.peek().getPrice() <= price)
-            stack.pop();
+            ans += stack.pop().getSpan();
 
-        ans = size - (!stack.empty() ? stack.peek().getIdx() + 1 : 0); 
+        // ans = size - (!stack.empty() ? stack.peek().getIdx() + 1 : 0); 
         
-        stack.push(new Span(price, size-1)); 
+        stack.push(new Span(price, ans)); 
         
         return ans;
     }
