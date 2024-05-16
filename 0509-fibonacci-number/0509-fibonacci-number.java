0647c5811dd1a56a0a14cbbf3b2f9c4988d738db
Anushka <anushkaa5000@gmail.com>
2024-05-17 03:58:11 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (9.48%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,16 @@
 class Solution {
+    Map<Integer, Integer> memo = new HashMap<>();
     public int fib(int n) {
+        if(memo.containsKey(n))
+            return memo.get(n);
         if(n == 0)
             return 0;
         
         if(n <= 2)
             return 1;
-        return fib(n-1) + fib(n-2);
+        
+        int fib = fib(n-1) + fib(n-2);
+        memo.put(n, fib);
+        return fib;
     }
 }
