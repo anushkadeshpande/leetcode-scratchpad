a5f82a16ae593ca0182034f040e18365c2393bb3
Anushka <anushkaa5000@gmail.com>
2024-06-04 01:29:46 +0530

    Time: 1 ms (99.91%), Space: 46.3 MB (19.50%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int xor = 0; 
-        for(int i=0; i<nums.length; i++) {
-            xor = xor ^ nums[i];
+        for(int num: nums) {
+            xor = xor ^ num;
         }
         
         return xor;
