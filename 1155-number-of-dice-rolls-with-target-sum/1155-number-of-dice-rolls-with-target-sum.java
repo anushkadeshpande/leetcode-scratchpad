690c1865fb35aef17df72693ed1778e6afbf16fa
Anushka <anushkaa5000@gmail.com>
2024-06-03 22:44:53 +0530

    Time: 9 ms (91.95%), Space: 41.1 MB (92.32%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,13 +1,8 @@
 class Solution {
-    Map<String, Integer> memo = new HashMap<>();
+    // Map<String, Integer> memo = new HashMap<>();
     
-    public int numRollsToTarget(int n, int k, int target) {
+    public int getNumberOfRolls(int n, int k, int target, int[][] memo){
         int ans = 0;
-        
-        String key = n + "_" + target;
-        
-        if(memo.containsKey(key))
-            return memo.get(key);
         
         // desired state, this is one of the possible ways
         if(n == 0 && target == 0)
@@ -16,15 +11,35 @@ class Solution {
         // dice are over but target is not met
         // or we still have dice and target becomes invalid
         // not a valid way, reject it
-        if(target < 0 || n <= 0)
+        if(n <= 0 || target <= 0)
             return 0;
         
+        
+        if(memo[n][target] != -1)
+            return memo[n][target];
+        
+        
         for(int i=0; i<k; i++) {
-            ans = (ans + numRollsToTarget(n-1, k, target-(i+1))) % 1000000007; 
+            ans = (ans + getNumberOfRolls(n-1, k, target-(i+1), memo)) % 1000000007; 
         }
         
-        memo.put(key, ans);
-        
+        // memo.put(key, ans);
+        memo[n][target] = ans;
         return ans;
     }
+    
+    public int numRollsToTarget(int n, int k, int target) {
+        
+        int[][] memo = new int[n+1][target+1];
+        
+        for(int i = 0; i <= n; i++){
+            Arrays.fill(memo[i], -1);
+        }
+        
+        return getNumberOfRolls(n, k, target, memo);
+        
+        // return 0;
+    }
+    
+    
 }
