a91f9c5128b9fca9b619cf6ea2165123f7ab5e20
Anushka <anushkaa5000@gmail.com>
2024-06-15 22:24:24 +0530

    Time: 23 ms (86.34%), Space: 56.1 MB (39.38%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,6 +1,5 @@
 class Solution {
-    Map<String, Boolean> map = new HashMap<>();
-    
+    int dp[][];
     public boolean canPartitionArray(int[] arr, int n, int target) {
         if(target == 0)
             return true;
@@ -8,25 +7,15 @@ class Solution {
         if(n <= 0)
             return false;
         
-        // if(dp[n][target] != -1)
-        //     return dp[n][target] == 1;
-        
-        String key = n + ":" + target;
-        
-        if(map.containsKey(key))
-            return map.get(key);
-        
-        // if(target >= arr[n-1])
-        //     dp[n][target] = (canPartitionArray(arr, n-1, target-arr[n-1]) || canPartitionArray(arr, n-1, target))? 1 : 0;
-        // else
-        //     dp[n][target] = canPartitionArray(arr, n-1, target) ? 1 : 0;
+        if(dp[n][target] != -1)
+            return dp[n-1][target] == 1;
         
         if(target >= arr[n-1])
-            map.put(key, (canPartitionArray(arr, n-1, target-arr[n-1]) || canPartitionArray(arr, n-1, target)));
+            dp[n][target] = (canPartitionArray(arr, n-1, target-arr[n-1]) || canPartitionArray(arr, n-1, target))? 1 : 0;
         else
-            map.put(key, canPartitionArray(arr, n-1, target));
-
-        return map.get(key);
+            dp[n][target] = canPartitionArray(arr, n-1, target) ? 1 : 0;
+        
+        return dp[n][target] == 1;
     }
     
     public boolean canPartition(int[] nums) {
@@ -36,11 +25,11 @@ class Solution {
         if(sum % 2 != 0)
           return false;
         
-//         dp = new int[nums.length+1][(sum/2) + 1];
+        dp = new int[nums.length+1][(sum/2) + 1];
         
-//         for(int i=0; i<=nums.length; i++)
-//             for(int j=0; j<=sum/2; j++)
-//                 dp[i][j] = -1;
+        for(int i=0; i<=nums.length; i++)
+            for(int j=0; j<=sum/2; j++)
+                dp[i][j] = -1;
         
         return canPartitionArray(nums, nums.length, sum/2);
     }
