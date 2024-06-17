2e0f9d71a58b03d8ed93a8a06f0d5f95611c4cb5
Anushka <anushkaa5000@gmail.com>
2024-06-17 14:03:44 +0530

    Time: 7 ms (13.34%), Space: 40.6 MB (14.02%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        // take closest sq root of c
+        // c - sq(closest_sq_root) = b^2
+        // if b is a perfect square, return true else false
+        int a = (int)Math.floor(Math.sqrt(c));
+        
+        // for every a from a to 0, check if we can get a b
+        for(int i=a; i>= 0; i--) {
+            int bSquare = c - ( i * i );
+        
+            double b = Math.sqrt(bSquare);
+            
+            if(Math.floor(b) == b)
+                return true;
+            
+        }
+
+        return false;
+    }
+}
