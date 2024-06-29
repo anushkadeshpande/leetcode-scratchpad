927562ac558e92daefba78485211340a0c6b1315
Anushka <anushkaa5000@gmail.com>
2024-06-30 02:24:35 +0530

    Time: 5 ms (87.17%), Space: 43.6 MB (40.08%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,5 +1,7 @@
 class Solution {
     
+    int[][] dp;
+    
     public int countSubarraySum(int[] nums, int n, int target) {
         if(target == 0 && n == 0)
             return 1;
@@ -7,10 +9,15 @@ class Solution {
         if(n <= 0)
             return 0;
         
+        if(dp[n][Math.abs(target)] != -1)
+            return dp[n][Math.abs(target)];
+        
         if(nums[n-1] <= target)
-            return countSubarraySum(nums, n-1, target-nums[n-1]) + countSubarraySum(nums, n-1, target);
+            dp[n][Math.abs(target)] = countSubarraySum(nums, n-1, target-nums[n-1]) + countSubarraySum(nums, n-1, target);
         else
-            return countSubarraySum(nums, n-1, target);
+            dp[n][Math.abs(target)] = countSubarraySum(nums, n-1, target);
+        
+        return dp[n][Math.abs(target)];
     }
     
     public int findTargetSumWays(int[] nums, int target) {
@@ -34,6 +41,13 @@ class Solution {
         
         if(tgtSum < target)
             return 0;
+        
+        dp = new int[n+1][Math.abs(tgtSum)+1];
+        
+        for(int i=0; i<=n; i++) {
+            for(int j=0; j<=tgtSum; j++)
+                dp[i][j] = -1;
+        }
 
         return countSubarraySum(nums, n, tgtSum);
     }
