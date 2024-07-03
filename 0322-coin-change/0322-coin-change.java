f549ec9028f06ed19529cf684c2608d59c569ce7
Anushka <anushkaa5000@gmail.com>
2024-07-03 23:23:37 +0530

    Time: 25 ms (34.07%), Space: 44.5 MB (43.72%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,17 +2,17 @@ class Solution {
     
     int[][] dp;
     
-    public int getMinCoins(int[] coins, int n, int amount) {
-        if(dp[n][amount] != -1)
-            return dp[n][amount];
+//     public int getMinCoins(int[] coins, int n, int amount) {
+//         if(dp[n][amount] != -1)
+//             return dp[n][amount];
         
-        if(coins[n-1] <= amount) 
-            dp[n][amount] = Math.min(1 + getMinCoins(coins, n, amount-coins[n-1]), getMinCoins(coins, n-1, amount));
-        else
-            dp[n][amount] = getMinCoins(coins, n-1, amount);
+//         if(coins[n-1] <= amount) 
+//             dp[n][amount] = Math.min(1 + getMinCoins(coins, n, amount-coins[n-1]), getMinCoins(coins, n-1, amount));
+//         else
+//             dp[n][amount] = getMinCoins(coins, n-1, amount);
         
-        return dp[n][amount];
-    }
+//         return dp[n][amount];
+//     }
     
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
@@ -23,12 +23,16 @@ class Solution {
                     dp[i][j] = 0;
                 else if(i == 0)
                     dp[i][j] = Integer.MAX_VALUE - 1;
-                else
-                    dp[i][j] = -1;
+                else {
+                    if(coins[i-1] <= j)
+                        dp[i][j] = Math.min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
+                    else
+                        dp[i][j] = dp[i-1][j];
+                }
             }
         }
-        int numCoins = getMinCoins(coins, coins.length, amount);
+        // int numCoins = getMinCoins(coins, coins.length, amount);
         
-        return numCoins == Integer.MAX_VALUE - 1 ? -1 : numCoins;
+        return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
     }
 }
