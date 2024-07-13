6150680e48b2971c0699274fd2a32bf16e1e3acf
Anushka <anushkaa5000@gmail.com>
2024-07-13 22:31:52 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (40.61%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,14 +1,17 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int cnt = 0;
-        int i = 1;
-        while(n > 0) {
-            n -=i;
-            if(n >= 0)
-                cnt++;
-            i++;
-        }
+//         int cnt = 0;
+//         int i = 1;
+//         while(n > 0) {
+//             n -=i;
+//             if(n >= 0)
+//                 cnt++;
+//             i++;
+//         }
         
-        return cnt;
+//         return cnt;
+        
+        // !USING GAUSS FORMULA
+        return (int) (Math.sqrt(1 + 8 * (long) n) - 1 ) / 2;
     }
 }
