407c98e0c0526ca219707c8b427b76ba53f70b2d
Anushka <anushkaa5000@gmail.com>
2024-07-14 00:38:07 +0530

    Time: 3 ms (11.26%), Space: 45.8 MB (33.77%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // find next greater element
+        // for every element, find max diff and add it to final answer
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        int n = prices.length;
+        
+        int[] nge = new int[n];
+        
+        for(int i=n-1; i>=0; i--) {
+            while(!stack.empty() && prices[stack.peek()] <= prices[i]) {
+                stack.pop();
+            }
+            
+            if(stack.empty())
+                nge[i] = -1;
+            else
+                nge[i] = stack.peek();
+            
+            stack.push(i);
+        }
+        
+        int ans = 0;
+        int curr = 0;
+        
+        int[] maxProfit = new int[n];
+        for(int i=0; i<n; i++)
+            maxProfit[i] = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(nge[i]!= -1)
+                maxProfit[nge[i]] = Math.max(prices[nge[i]] - prices[i], maxProfit[nge[i]]);
+        }
+        
+        for(int i=0; i<n; i++)
+            ans += maxProfit[i];
+        return ans;
+    }
+}
