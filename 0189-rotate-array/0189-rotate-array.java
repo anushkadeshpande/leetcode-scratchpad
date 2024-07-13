88a5bf6d2d469389e4463a669702699abb8faef0
Anushka <anushkaa5000@gmail.com>
2024-07-14 03:10:25 +0530

    Time: 2 ms (27.71%), Space: 57.3 MB (40.87%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,18 +1,20 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         
+        int n = nums.length;
+        
         // normalize k
-        k = k % nums.length;
+        k = k % n;
         
         // get last k elements
         int[] lastKElements = new int[k];
         int j = 0;
-        for(int i= nums.length -k; i < nums.length; i++) {
+        for(int i= n -k; i < n; i++) {
             lastKElements[j++] = nums[i];
         }
         
         // shift array by k places
-        for(int i = nums.length - 1; i >= k ; i--) {
+        for(int i = n - 1; i >= k ; i--) {
             nums[i] = nums[i-k];
         }
         
