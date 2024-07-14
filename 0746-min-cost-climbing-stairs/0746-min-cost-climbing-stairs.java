53ebd78a3432207041133069dc43edf06d82531d
Anushka <anushkaa5000@gmail.com>
2024-07-14 15:28:22 +0530

    Time: 1 ms (42.37%), Space: 42.8 MB (67.95%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -2,17 +2,17 @@ class Solution {
     
     int[] dp;
     
-    public int getMinCostClimb(int[] cost, int idx) {
-        if(idx >= cost.length)
+    public int getMinCostClimb(int[] cost, int n, int idx) {
+        if(idx >= n)
             return 0;
          
         if(dp[idx] != -1)
             return dp[idx];
         // pay and climb one step
-        int oneStepCost = cost[idx] + getMinCostClimb(cost, idx + 1);
+        int oneStepCost = cost[idx] + getMinCostClimb(cost, n, idx + 1);
         
         // pay and climb 2 steps
-        int twoStepCost = cost[idx] + getMinCostClimb(cost, idx+2);
+        int twoStepCost = cost[idx] + getMinCostClimb(cost, n, idx+2);
         
         dp[idx] = Math.min(oneStepCost, twoStepCost);
         
@@ -21,11 +21,11 @@ class Solution {
     
     public int minCostClimbingStairs(int[] cost) {
         // at every step, we either climb it or skip it
-        
-        dp = new int[cost.length + 1];
-        for(int i=0; i<=cost.length; i++) {
+        int n = cost.length;
+        dp = new int[n + 1];
+        for(int i=0; i<=n; i++) {
             dp[i] = -1;
         }
-        return Math.min(getMinCostClimb(cost, 0), getMinCostClimb(cost, 1));
+        return Math.min(getMinCostClimb(cost, n, 0), getMinCostClimb(cost, n, 1));
     }
 }
