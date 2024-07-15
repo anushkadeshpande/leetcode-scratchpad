a36e0560d6df233ace539dfe49ef43754c73b614
Anushka <anushkaa5000@gmail.com>
2024-07-15 23:26:09 +0530

    Time: 2 ms (97.19%), Space: 42.6 MB (95.65%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -6,14 +6,16 @@ class Solution {
         if(n != m)
             return false;
         
-        char[] s1 = s.toCharArray();
-        Arrays.sort(s1);
+        int[] count = new int[26];
         
-        char[] s2 = t.toCharArray();
-        Arrays.sort(s2);
+        for(int i=0; i<n; i++)
+            count[s.charAt(i) - 'a']++;
         
-        for(int i=0; i<n; i++) {
-            if(s1[i] != s2[i])
+        for(int i=0; i<m; i++)
+            count[t.charAt(i) - 'a']--;
+        
+        for(int i=0; i<26; i++) {
+            if(count[i] > 0)
                 return false;
         }
         
