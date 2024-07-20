d760eca6e8453bbdaea0dd94fee559fabe4a3a81
Anushka <anushkaa5000@gmail.com>
2024-07-20 22:56:26 +0530

    Time: 22 ms (64.98%), Space: 50.6 MB (17.85%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -9,8 +9,9 @@ class Solution {
             for(int j=0; j<=n; j++) {
                 if(i == 0 || j == 0)
                     dp[i][j] = 0;
-                else if(str1.charAt(i-1) == str2.charAt(j-1))
+                else if(str1.charAt(i-1) == str2.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1] + 1;
+                }
                 else
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
             }
@@ -18,33 +19,39 @@ class Solution {
         
         int i = m;
         int j = n;
-        String ans = "";
+        // String ans = "";
+        StringBuilder ans = new StringBuilder();
         while(i!=0 && j != 0) {
             if(str1.charAt(i-1) == str2.charAt(j-1)) {
-                ans = str1.charAt(i-1) + ans;
+                // ans = str1.charAt(i-1) + ans;
+                ans.append(str1.charAt(i-1));
                 i--;
                 j--;
             } else if(dp[i-1][j] > dp[i][j-1]) {
-                ans = str1.charAt(i-1) + ans;
+                // ans = str1.charAt(i-1) + ans;
+                ans.append(str1.charAt(i-1));
                 i--;
                 
             } else {
-                ans = str2.charAt(j-1) + ans;
+                // ans = str2.charAt(j-1) + ans;
+                ans.append(str2.charAt(j-1));
                 j --;
                 
             }
         }
         
         while(i > 0) {
-            ans = str1.charAt(i-1) + ans;
+            // ans = str1.charAt(i-1) + ans;
+            ans.append(str1.charAt(i-1));
             i--;
         }
         
         while(j > 0) {
-            ans = str2.charAt(j-1) + ans;
+            // ans = str2.charAt(j-1) + ans;
+            ans.append(str2.charAt(j-1));
             j--;
         }
         
-        return ans;
+        return ans.reverse().toString();
     }
 }
