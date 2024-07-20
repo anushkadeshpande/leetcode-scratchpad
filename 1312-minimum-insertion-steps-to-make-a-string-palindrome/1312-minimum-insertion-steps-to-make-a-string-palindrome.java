dac3768dc37b83cd7821a493f63a48c4faaddd05
Anushka <anushkaa5000@gmail.com>
2024-07-20 19:43:38 +0530

    Time: 26 ms (59.24%), Space: 45.5 MB (20.53%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -3,7 +3,7 @@ class Solution {
         // find longest palindromic subsequence
         int n = s.length();
         int[][] dp = new int[n+1][n+1];
-        int maxLength = 0;
+        
         for(int i=0; i<=n; i++) {
             for(int j=0; j<=n; j++) {
                 if(i == 0 || j == 0)
