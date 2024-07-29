2f47978a131640564229e8b7ad82e311f82bd2a8
Anushka <anushkaa5000@gmail.com>
2024-07-29 06:34:30 +0530

    Time: 606 ms (5.00%), Space: 45.5 MB (99.03%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,24 @@
+class Solution {
+    static boolean[] isPrime = new boolean[5_000_001];
+    
+    public Solution() {
+        for(int i = 2; i*i <= 5_000_000; i++){
+            
+            if(isPrime[i] == false){
+                for(int j = i * i; j <= 5_000_000; j += i){
+                    isPrime[j] = true;
+                }
+            }
+        }
+    }
+    
+    public int countPrimes(int n) {
+        int count = 0;
+        for(int i=2; i<n; i++) {
+            if(!isPrime[i])
+                count++;
+        }
+        
+        return count;
+    }
+}
