69f7b8da40f6ddbc952e7cc65e1d2b876e0eb0dc
Anushka <anushkaa5000@gmail.com>
2024-08-02 04:28:42 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (58.09%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -2,14 +2,25 @@ class Solution {
     public int countSeniors(String[] details) {
         // int count = 0;
         
+        // 1. substring : faster than 82.65
         // for(String detail: details) {
         //     if(Integer.parseInt(detail.substring(11, 13)) > 60)
         //         count++;
         // }
-        
-        return (int)Arrays.stream(details).filter(detail -> Integer.parseInt(detail.substring(11, 13)) > 60).count();
-        
-        
         // return count;
+        
+        
+        // 2. Stream, even slower, 11.44
+        // return (int)Arrays.stream(details).filter(detail -> Integer.parseInt(detail.substring(11, 13)) > 60).count();
+        
+        
+        // 3. charAt
+        int count = 0;
+        for(String detail: details) {
+            if((((detail.charAt(11) - '0') * 10) + (detail.charAt(12) - '0')) > 60)
+                count++;
+        }
+        return count;
+        
     }
 }
