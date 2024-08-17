e1dc9373d5ba5861ca1bdafa929266f4630a5a87
Anushka <anushkaa5000@gmail.com>
2024-08-17 18:26:24 +0530

    Time: 1 ms (92.71%), Space: 41.4 MB (52.76%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -2,22 +2,42 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         // find lcs of both strings
         // if lcs == s ? true : false
+//         int n = s.length();
+//         int m = t.length();
+        
+//         int[][] dp = new int[n+1][m+1];
+        
+//         for(int i=0; i<=n; i++) {
+//             for(int j=0; j<=m; j++) {
+//                 if(i == 0 || j == 0)
+//                     dp[i][j] = 0;
+//                 else if(s.charAt(i-1) == t.charAt(j-1)) {
+//                     dp[i][j] = 1 + dp[i-1][j-1];
+//                 } else
+//                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+//             }
+//         }
+        
+//         return dp[n][m] == n ? true : false;
+        
+        int i = 0;
+        int j = 0;
         int n = s.length();
         int m = t.length();
         
-        int[][] dp = new int[n+1][m+1];
-        
-        for(int i=0; i<=n; i++) {
-            for(int j=0; j<=m; j++) {
-                if(i == 0 || j == 0)
-                    dp[i][j] = 0;
-                else if(s.charAt(i-1) == t.charAt(j-1)) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        while(i < n && j < m) {
+            if(s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
             }
+                
         }
         
-        return dp[n][m] == n ? true : false;
+        if(i == n)
+            return true;
+        else
+            return false;
     }
 }
