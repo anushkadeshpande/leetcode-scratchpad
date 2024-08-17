2ae373a51c223d05090be00c3be2fb55c8c6a8c1
Anushka <anushkaa5000@gmail.com>
2024-08-18 00:55:15 +0530

    Time: 2307 ms (47.22%), Space: 80.9 MB (5.47%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -4,20 +4,18 @@ class Solution {
     
     public int[][] palindromeDp;
     
-    public String s1;
-    
-    public int getMinCuts(int i, int j) {
+    public int getMinCuts(String s, int i, int j) {
         if(dp[i][j] != -1)
             return dp[i][j];
         
-        if(i >= j || isPalindrome(i, j))
+        if(i >= j || isPalindrome(s, i, j))
             return 0;
         
         int minCuts = Integer.MAX_VALUE;
         
         for(int k=i; k<j; k++) {
-            if(isPalindrome(i, k)) {
-                int cuts = 1 + getMinCuts(k+1, j);
+            if(isPalindrome(s, i, k)) {
+                int cuts = 1 + getMinCuts(s, k+1, j);
                 minCuts = Math.min(minCuts, cuts);    
             }
             
@@ -34,17 +32,16 @@ class Solution {
         dp = new int[n+1][n+1];
         palindromeDp = new int[n+1][n+1];
         
-        s1 = s;
         for(int i=0; i<=n; i++)
             for(int j=0; j<=n; j++) {
                     dp[i][j] = -1;
                     palindromeDp[i][j] = -1;
             }
         
-        return getMinCuts(0, s.length()-1);
+        return getMinCuts(s, 0, s.length()-1);
     }
     
-    public boolean isPalindrome(int i, int j) {
+    public boolean isPalindrome(String s, int i, int j) {
         if(palindromeDp[i][j] != -1)
             return palindromeDp[i][j] == 1;
         
@@ -52,7 +49,7 @@ class Solution {
         int q = j;
         
         while(p < q) {
-            if(s1.charAt(p) != s1.charAt(q)) {
+            if(s.charAt(p) != s.charAt(q)) {
                 palindromeDp[i][j] = 0;
                 
                 return false;
