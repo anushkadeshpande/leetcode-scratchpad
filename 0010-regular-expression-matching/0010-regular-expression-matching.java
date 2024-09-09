e364612018f91d59413d3f4f13518b4446b5c803
Anushka <anushkaa5000@gmail.com>
2024-09-09 09:56:57 +0530

    Time: 436 ms (20.08%), Space: 41.4 MB (99.03%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,44 +1,35 @@
 class Solution {
     public boolean checkMatch(String s, String p, int sIndex, int pIndex) {
-//         if(sIndex == s.length() && pIndex == p.length())
-//             return true;
-        
-//         if(pIndex > s.length())
-//             return false;
-        
-//         boolean match = false;
-        
-//         // either the character matches or there is a dot
-//         if(sIndex < s.length() && (s.charAt(sIndex) == p.charAt(pIndex) || p.charAt(pIndex) == '.')) {
-//             match = true;
-//         }
-        
-//         // even if the character matched, if the next char is * 
-//         // we either consider it or ignore it
-//         if(pIndex + 1 < p.length() && p.charAt(pIndex + 1) == '*') {
-//             return (match && checkMatch(s, p, sIndex+1, pIndex)) || checkMatch(s,p,sIndex,pIndex+2);
-//         } 
-        
-//         if(match)
-//             return checkMatch(s, p, sIndex+1, pIndex+1);
-        
         // if char match go ahead
         // if dot, skip one char
         // if *, either use it or ignore it
+        
+        // we've reached the end of both s and p, so everything has matched
         if(sIndex >= s.length() && pIndex >= p.length())
             return true;
+        
+        // we've exhausted the pattern, but are still left with the string, hence, invalid
         if(pIndex >= p.length())
             return false;
+        
+        
         boolean match = false;
+        // if characters match, it could be a match
+        // we can also have a *, in that case we need to further check it
+        // hence, we cannot directly return true here
         if(sIndex < s.length() && s.charAt(sIndex) == p.charAt(pIndex))
             match = true;
+        
+        // if it is a '.', whatever is the char, it could match, but need further checking if the next char is '*'
         else if(sIndex < s.length() && p.charAt(pIndex) == '.')
             match = true;
         
+        // if next char is char, we consider it or skip it
+        // if we consider it, the current match should also be true
         if(pIndex+1 < p.length() && p.charAt(pIndex+1) == '*')
             return (match && checkMatch(s, p, sIndex+1, pIndex)) || checkMatch(s, p, sIndex, pIndex+2);
         
-        
+        // if the next char is not * and we've already matched the chars, check the next chars in s and p
         if(match == true)
             return checkMatch(s, p, sIndex+1, pIndex+1);
         
@@ -47,46 +38,5 @@ class Solution {
     
     public boolean isMatch(String s, String p) {
         return checkMatch(s, p, 0, 0);
-//         int j = 0;
-//         for(int i=0; i<p.length(); i++) {
-//             char regExChar = p.charAt(i);
-//             char stringChar = s.charAt(j);
-            
-//             if(i != p.length() -1 && p.charAt(i+1) == '*'){
-//                 char previousChar;
-//                 // if(i != 0) {
-//                 previousChar = p.charAt(i); 
-//                 char nextChar;
-                
-//                 nextChar = p.charAt(i+1);
-//                 // } else {
-//                     // previousChar = '.';
-//                 // }
-//                 if(previousChar == '.' && Character.isAlphabetic(nextChar))
-//                     return true;
-                
-//                 else
-//                 // System.out.println(previousChar);
-                
-//                 while(j < s.length() && s.charAt(j) == previousChar) {
-//                     j++;
-//                 }
-//                 // System.out.println(j);
-//                 i++;
-//             } 
-            
-//             else if(Character.isAlphabetic(regExChar) && regExChar == stringChar)
-//                 j++;
-//             else if(regExChar == '.')
-//                 j++;
-//             // else if(regExChar == '*')
-//             else
-//                 return false;
-//         }
-        
-//         if(j < s.length())
-//             return false;
-        
-//         return true;
     }
 }
