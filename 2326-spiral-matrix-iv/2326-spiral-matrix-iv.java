7b7fe6d0d861bbe78a5b478d946fbbd0ac22ab66
Anushka <anushkaa5000@gmail.com>
2024-09-09 08:55:57 +0530

    Time: 6 ms (97.69%), Space: 56.8 MB (49.51%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.java
@@ -0,0 +1,95 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        int i=0;
+        int j=0;
+        int[][] matrix = new int[m][n];
+        
+        int top = 0;
+        int bottom = m-1;
+        
+        int left = 0;
+        int right = n-1;
+        
+        for(i=0; i<m; i++) {
+            for(j =0; j<n; j++)
+                matrix[i][j] = -1;
+        }
+        i=0;
+        j=0;
+
+        while(head != null) {
+            
+            i = left;
+            while(head != null && i <= right) {
+                matrix[top][i] = head.val;
+                head = head.next;
+                i++;
+            } 
+            
+            // System.out.println("After STEP 1");
+            // for(int k=0; k<m; k++) {
+            //     for(int p=0; p<n; p++) {
+            //         System.out.print(matrix[k][p] + "\t");
+            //     }
+            //     System.out.println();
+            // }
+            
+            top++;
+            i = top;
+            
+            while(head != null && i <= bottom) {
+                matrix[i][right] = head.val;
+                head = head.next;
+                i++;
+            }
+            right--;
+            i = right;
+            
+            // System.out.println("After STEP 2");
+            // for(int k=0; k<m; k++) {
+            //     for(int p=0; p<n; p++) {
+            //         System.out.print(matrix[k][p] + "\t");
+            //     }
+            //     System.out.println();
+            // }
+
+            while(head != null && i >= left) {
+                matrix[bottom][i] = head.val;
+                head = head.next;
+                i--;
+            }
+            
+            bottom--;
+            i=bottom;
+            
+            // System.out.println("After STEP 3");
+            // for(int k=0; k<m; k++) {
+            //     for(int p=0; p<n; p++) {
+            //         System.out.print(matrix[k][p] + "\t");
+            //     }
+            //     System.out.println();
+            // }
+            
+            while(head != null && i >= top) {
+                matrix[i][left] = head.val;
+                head = head.next;
+                i--;
+            }
+            
+            left++;
+
+        }
+        
+        return matrix;
+    }
+}
