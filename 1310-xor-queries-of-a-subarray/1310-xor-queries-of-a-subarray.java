3b1254fe0ae5d5ebc98f4f2b5903c8f475c4dd2e
Anushka <anushkaa5000@gmail.com>
2024-09-13 09:08:41 +0530

    Time: 2 ms (100.00%), Space: 57.6 MB (55.23%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -3,16 +3,45 @@ class Solution {
         int n = queries.length;
         int[] answer = new int[n];
         
+//         for(int i=0; i<n; i++) {
+//             int start = queries[i][0];
+//             int end = queries[i][1];
+            
+//             int xor = arr[start];
+//             for(int j=start+1; j<=end; j++) {
+//                 xor = xor ^ arr[j];
+//             }
+            
+//             answer[i] = xor;
+//         }
+        
+//         return answer;
+        
+         /* 
+         *
+         *
+         * BETTER SOLUTION USING PREFIX SUM
+         *
+         */
+        
+        int m = arr.length;
+        int[] prefix = new int[m];
+        prefix[0] = arr[0];
+        for(int i=1; i<m; i++) {
+            prefix[i] = prefix[i-1] ^ arr[i];
+        }
+        
         for(int i=0; i<n; i++) {
             int start = queries[i][0];
             int end = queries[i][1];
             
-            int xor = arr[start];
-            for(int j=start+1; j<=end; j++) {
-                xor = xor ^ arr[j];
+            if(start == 0) {
+                answer[i] = prefix[end];
+            } else {
+                // remove prefix[0] - prefix[start-1] from prefix[end]
+                // this will give prefix[start] - prefix[end]
+                answer[i] = prefix[start-1] ^ prefix[end];
             }
-            
-            answer[i] = xor;
         }
         
         return answer;
