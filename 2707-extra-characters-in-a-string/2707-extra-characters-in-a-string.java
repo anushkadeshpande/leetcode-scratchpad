2f16d7db93fcbd8a9df6da39a5245f05e29a32ce
Anushka <anushkaa5000@gmail.com>
2024-10-12 01:55:55 +0530

    Time: 8 ms (94.16%), Space: 45.2 MB (53.76%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -1,51 +1,68 @@
-class Node{
-    public Node[] child;
-    public boolean isEnd;
-    public Node(){
-        child = new Node[26];
-        isEnd = false;
-    }
-}
 class Solution {
-    public int minExtraChar(String s, String[] dictionary) {
-        Node root = new Node();
-        for(String str: dictionary){
-            insert(str, root);
-        }
-        int n = s.length();
-        int[] dp = new int[n+1];
-        for(int i = 0; i < n+1; i++){
-            dp[i] = i;
-        }
+    class Node{
+        Node[] links = new Node[26];
+        boolean flag = false;
+    }
 
-        for(int i = 0; i < n; i++){
-            search(s, root, i, dp);
-            dp[i+1] = Math.min(dp[i+1], dp[i]+1);
+    class Trie {
+        Node root;
+        
+        public Trie() {
+            root = new Node();
         }
-        return dp[n];
-    }
-    public void insert(String s, Node root){
-        for(int i = 0; i < s.length(); i++){
-            int path = s.charAt(i) - 'a';
-            if(root.child[path] == null){
-                Node node = new Node();
-                root.child[path] = node;
+        
+        public void insert(String s) {
+            Node temp = root;
+            
+            for(int i=0; i<s.length(); i++) {
+                char ch = s.charAt(i);
+                if(temp.links[ch - 'a'] == null)
+                    temp.links[ch - 'a'] = new Node();
+                temp = temp.links[ch - 'a'];
             }
-            root = root.child[path];
+            temp.flag = true;
         }
-        root.isEnd = true;
     }
-    public void search(String s, Node root, int idx, int[] dp){
-        for(int i = idx; i < s.length(); i++){
-            int path = s.charAt(i) - 'a';
-            if(root.child[path] != null){
-                root = root.child[path];
-                if(root.isEnd){
-                    dp[i+1] = Math.min(dp[i+1], dp[idx]);
-                }
-            } else {
-                return;
-            }
+    
+    int[] dp;
+    
+    public int dfs(int i, String s, Trie trie) {
+        if(i == s.length())
+            return 0;
+        
+        if(dp[i] != -1)
+            return dp[i];
+        
+        int res = -1;
+        // skip current char
+        res = 1 + dfs(i+1, s, trie);
+        
+        // take current char
+        Node curr = trie.root;
+        
+        for(int j=i; j<s.length(); j++) {
+            char ch = s.charAt(j);
+            if(curr.links[ch - 'a'] == null)
+                break;
+            curr = curr.links[ch - 'a'];
+            if(curr.flag)
+                res = Math.min(res, dfs(j+1, s, trie));
         }
+        
+        dp[i] = res;
+        return res;
+    }
+    
+    public int minExtraChar(String s, String[] dictionary) {
+       Trie trie = new Trie();
+        dp = new int[s.length()];
+        for(int i=0; i<s.length(); i++)
+            dp[i] = -1;
+        
+        // insert all of the words in the trie
+        for(String word: dictionary)
+            trie.insert(word);
+        
+        return dfs(0, s, trie);
     }
 }
