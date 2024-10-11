5ef590cff21f06e1ad1877d7a557e1fbbe60240b
Anushka <anushkaa5000@gmail.com>
2024-10-12 00:43:58 +0530

    Time: 2 ms (85.30%), Space: 45.9 MB (33.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,7 +3,7 @@ class Solution {
         int idx = 0;
         int n = nums.length;
         
-        for(int i=0; i<nums.length; i++) {
+        for(int i=0; i<n; i++) {
             if(nums[i] != 0) {
                 nums[idx++] = nums[i];
             }
