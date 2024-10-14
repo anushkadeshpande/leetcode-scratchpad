813bd98efbff4373bba81569110a867e58425b10
Anushka <anushkaa5000@gmail.com>
2024-10-15 01:11:44 +0530

    Time: 4 ms (86.50%), Space: 58 MB (69.70%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,5 +1,7 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+
+        // METHOD 1
 //         Set<Integer> set = new HashSet<>();
         
 //         for(int n: nums) {
@@ -10,16 +12,42 @@ class Solution {
 //         }
          
 //         return -1;
-        for(int i=0; i<nums.length; i++) {
-            int idx = Math.abs(nums[i]);
-            if(nums[idx]< 0) {
-                return idx;
-            }
+
+        
+        // METHOD 2
+//         for(int i=0; i<nums.length; i++) {
+//             int idx = Math.abs(nums[i]);
+//             if(nums[idx]< 0) {
+//                 return idx;
+//             }
             
-            // mark current number as visited
-            nums[idx] = -nums[idx];
+//             // mark current number as visited
+//             nums[idx] = -nums[idx];
+//         }
+        
+        
+        
+        // METHOD 3
+        // linked list cycle method
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        // while(slow != fast) {
+        //     slow = nums[slow];
+        //     fast = nums[nums[fast]];
+        // }
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        fast = nums[0];
+        
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
         
-        return -1;
+        return slow;
     }
 }
