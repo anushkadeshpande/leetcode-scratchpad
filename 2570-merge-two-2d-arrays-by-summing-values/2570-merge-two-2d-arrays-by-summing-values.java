c8e925a995e3b1bc37aac2ecd519cc0b105efbf1
Anushka <anushkaa5000@gmail.com>
2025-03-03 00:14:25 +0530

    Time: 2 ms (52.4%), Space: 45.2 MB (78.89%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
@@ -3,68 +3,72 @@ class Solution {
         int m = nums1.length;
         int n = nums2.length;
 
-        // int[][] res = new int[m+n][2];
         List<List<Integer>> res = new ArrayList<>();
 
         int i = 0;
         int j = 0;
-        int k = 0;
 
         while(i < m && j < n) {
             List<Integer> subList = new ArrayList<>();
             if(nums1[i][0] < nums2[j][0]) {
-                // res[k][0] = nums1[i][0];
-                subList.add(nums1[i][0]);
-                // res[k][1] = nums1[i][1];
-                subList.add(nums1[i][1]);
-                // res.add(Arrays.asList(nums1[i][0], nums1[i][1]));
+                // subList.add(nums1[i][0]);
+                // subList.add(nums1[i][1]);
+                res.add(Arrays.asList(nums1[i][0], nums1[i][1]));
                 i++;
             } else if(nums2[j][0] < nums1[i][0]) {
                 // res[k][0] = nums2[j][0];
-                subList.add(nums2[j][0]);
+                // subList.add(nums2[j][0]);
                 // res[k][1] = nums2[j][1];
-                subList.add(nums2[j][1]);
-                // res.add(Arrays.asList(nums1[j][0], nums2[j][1]));
+                // subList.add(nums2[j][1]);
+                res.add(Arrays.asList(nums2[j][0], nums2[j][1]));
 
                 j++;
             } else {
                 // res[k][0] = nums2[j][0];
-                subList.add(nums1[i][0]);
+                // subList.add(nums1[i][0]);
                 // res[k][1] = nums1[i][1] + nums2[j][1];
-                subList.add(nums1[i][1] + nums2[j][1]);
+                // subList.add(nums1[i][1] + nums2[j][1]);
+                res.add(Arrays.asList(nums1[i][0], nums1[i][1] + nums2[j][1]));
+
                 i++;
                 j++;
             }
             
             // k++;
-                res.add(subList);
+                // res.add(subList);
 
         }
 
         while(i < m) {
-            List<Integer> subList = new ArrayList<>();
+            // List<Integer> subList = new ArrayList<>();
 
                 // res[k][0] = nums1[i][0];
-                subList.add(nums1[i][0]);
+                // subList.add(nums1[i][0]);
                 // res[k][1] = nums1[i][1];
-                subList.add(nums1[i][1]);
+                // subList.add(nums1[i][1]);
+                res.add(Arrays.asList(nums1[i][0], nums1[i][1]));
+
                 i++;
                 // k++;
 
-                res.add(subList);
+                // res.add(subList);
         }
 
         while(j < n) {
-                        List<Integer> subList = new ArrayList<>();
+                        // List<Integer> subList = new ArrayList<>();
 
                 // res[k][0] = nums2[j][0];
-                subList.add(nums2[j][0]);
+                // subList.add(nums2[j][0]);
 
                 // res[k][1] = nums2[j][1];
-                subList.add(nums2[j][1]);
+                // subList.add(nums2[j][1]);
+                               res.add(Arrays.asList(nums2[j][0], nums2[j][1]));
 
                 j++;
-                                res.add(subList);
+                                // res.add(subList);
+
+ 
+
 
                 // k++;
         }
