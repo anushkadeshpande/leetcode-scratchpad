933a9308e0ffc20400a519ce6985d7da9856ee73
Anushka <anushkaa5000@gmail.com>
2025-09-11 22:47:53 +0530

    Time: 16 ms (91.96%), Space: 45.7 MB (91.75%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -1,23 +1,53 @@
 class Solution {
     public String sortVowels(String s) {
-        List<Character> vowels = new ArrayList<>();
+        // List<Character> vowels = new ArrayList<>();
 
+        // for(char c: s.toCharArray()) {
+        //     if(isVowel(c))
+        //         vowels.add(c);
+        // }
+
+        // Collections.sort(vowels);
+        
+        // int j = 0;
+        // StringBuilder sb = new StringBuilder(s);
+        
+        // for(int i=0; i<s.length(); i++) {
+        //     if(isVowel(s.charAt(i))) 
+        //         sb.setCharAt(i, vowels.get(j++));     
+        // }
+
+        // return sb.toString();
+
+
+        // Approach-2 
+
+        int[] count = new int[100];
+
+        // store frequencies for each vowel
         for(char c: s.toCharArray()) {
             if(isVowel(c))
-                vowels.add(c);
+                count[c - 'A']++;
         }
 
-        Collections.sort(vowels);
-        
+        StringBuilder sortedString = new StringBuilder();
+        String sortedVowels = "AEIOUaeiou";
         int j = 0;
-        StringBuilder sb = new StringBuilder(s);
-        
-        for(int i=0; i<s.length(); i++) {
-            if(isVowel(s.charAt(i))) 
-                sb.setCharAt(i, vowels.get(j++));     
+
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(!isVowel(c))
+                sortedString.append(c);
+            else {
+                while(count[sortedVowels.charAt(j) - 'A'] == 0)
+                    j++;
+                
+                sortedString.append(sortedVowels.charAt(j));
+                count[sortedVowels.charAt(j) - 'A']--;
+            }
         }
 
-        return sb.toString();
+        return sortedString.toString();
     }
 
     public boolean isVowel(char c) {
