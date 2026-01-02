6d66eeed03c9561af803c23a7f1e63af345e90b7
Anushka <anushkaa5000@gmail.com>
2026-01-02 15:26:24 +0530

    Time: 0 ms (100%), Space: 48 MB (6.11%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,23 +1,33 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
+        // only one element will be repeated in the array
+        Set<Integer> set = new HashSet<>();
+
+        for(int n: nums) {
+            if(!set.add(n))
+                return n;
+        } 
+
+        return 0;
+
         // figure out what is n
         // sort arr
         // check which number appears n times
 
-        int n = nums.length/2;
-        Arrays.sort(nums);
+        // int n = nums.length/2;
+        // Arrays.sort(nums);
 
-        int cnt=1;
-        for(int i=1; i<nums.length; i++) {
-            if(nums[i] == nums[i-1]) {
-                cnt++;
-                if(cnt == n)
-                    return nums[i];
-            }
-            else
-                cnt = 1;
-        }
+        // int cnt=1;
+        // for(int i=1; i<nums.length; i++) {
+        //     if(nums[i] == nums[i-1]) {
+        //         cnt++;
+        //         if(cnt == n)
+        //             return nums[i];
+        //     }
+        //     else
+        //         cnt = 1;
+        // }
 
-        return 0;
+        // return 0;
     }
 }
