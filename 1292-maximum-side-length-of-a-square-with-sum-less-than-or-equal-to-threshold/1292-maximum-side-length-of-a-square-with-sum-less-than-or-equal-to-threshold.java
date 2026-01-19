ce6a4b417e5a49d1fedabc60f0c071db80328e97
Anushka <anushkaa5000@gmail.com>
2026-01-19 21:33:46 +0530

    Time: 41 ms (10.15%), Space: 53.6 MB (94.42%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,14 +1,22 @@
 class Solution {
+    int[][] prefix;
+
     public int maxSideLength(int[][] mat, int threshold) {
         int m = mat.length;
         int n = mat[0].length;
+
+        // compute prefix matrix
+        prefix = new int[m][n];
+
+        computePrefix(mat, m, n);
+
         int bestSide = 0;
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 // check for all offsets
                 int maxPossibleLength = Math.min(m-i, n-j);
                 for(int k=0; k<maxPossibleLength; k++) {
-                    int val = findSum(mat, i, j, i+k, j+k);
+                    int val = findSum(i, j, i+k, j+k);
 
                     if(val <= threshold) {
                         bestSide = Math.max(bestSide, k+1);
@@ -21,14 +29,27 @@ class Solution {
         return bestSide;
     }
 
-    int findSum(int[][] mat, int r1, int c1, int r2, int c2) {
-        int sum = 0;
-        for(int i=r1; i<=r2; i++) {
-            for(int j=c1; j<=c2; j++) {
-                sum += mat[i][j];
+    void computePrefix(int[][] mat, int m, int n) {
+        // prefix[0][0] = mat[0][0];
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                prefix[i][j] = (i > 0? prefix[i-1][j] : 0 ) + (j > 0 ? prefix[i][j-1] : 0) -
+                                ((i > 0 && j > 0) ? prefix[i-1][j-1]:0) + mat[i][j];
             }
         }
+    }
 
-        return sum;
+    int findSum(int r1, int c1, int r2, int c2) {
+        int sum = 0;
+        // for(int i=r1; i<=r2; i++) {
+        //     for(int j=c1; j<=c2; j++) {
+        //         sum += mat[i][j];
+        //     }
+        // }
+
+        return prefix[r2][c2] 
+            - (r1 > 0? prefix[r1-1][c2] : 0)
+            - (c1 > 0? prefix[r2][c1-1] : 0) 
+            + (r1 > 0 && c1> 0? prefix[r1-1][c1-1] : 0);
     }
 }
